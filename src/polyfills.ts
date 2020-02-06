836149f5323325e57d821702cdf05740c6eb383a
Jeremy Quick <jrquick@uab.edu>
2020-02-05 21:07:14 -0600

    Add PhantomJS

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,6 +17,20 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/weak-map';
+import 'core-js/es6/set';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
@@ -60,3 +74,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'hammerjs';
+
+import 'intl';
+import 'intl/locale-data/jsonp/en';
