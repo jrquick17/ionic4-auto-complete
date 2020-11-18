a85fd6a0b79d095621bb4ee1b9e25b36a81571a6
Jeremy Quick <jrquick@uab.edu>
2020-11-18 17:26:03 -0600

    build

--- demo/www/ngsw-worker.js
+++ demo/www/ngsw-worker.js
@@ -244,6 +244,12 @@
             return `${error}`;
         }
     }
+    class SwUnrecoverableStateError extends SwCriticalError {
+        constructor() {
+            super(...arguments);
+            this.isUnrecoverableState = true;
+        }
+    }
 
     /**
      * @license
@@ -730,8 +736,8 @@
                     // reasons: either the non-cache-busted request failed (hopefully transiently) or if the
                     // hash of the content retrieved does not match the canonical hash from the manifest. It's
                     // only valid to access the content of the first response if the request was successful.
-                    let makeCacheBustedRequest = networkResult.ok;
-                    if (makeCacheBustedRequest) {
+                    let makeCacheBustedRequest = !networkResult.ok;
+                    if (networkResult.ok) {
                         // The request was successful. A cache-busted request is only necessary if the hashes
                         // don't match. Compare them, making sure to clone the response so it can be used later
                         // if it proves to be valid.
@@ -749,7 +755,12 @@
                         const cacheBustedResult = yield this.safeFetch(cacheBustReq);
                         // If the response was unsuccessful, there's nothing more that can be done.
                         if (!cacheBustedResult.ok) {
-                            throw new SwCriticalError(`Response not Ok (cacheBustedFetchFromNetwork): cache busted request for ${req.url} returned response ${cacheBustedResult.status} ${cacheBustedResult.statusText}`);
+                            if (cacheBustedResult.status === 404) {
+                                throw new SwUnrecoverableStateError(`Failed to retrieve hashed resource from the server. (AssetGroup: ${this.config.name} | URL: ${url})`);
+                            }
+                            else {
+                                throw new SwCriticalError(`Response not Ok (cacheBustedFetchFromNetwork): cache busted request for ${req.url} returned response ${cacheBustedResult.status} ${cacheBustedResult.statusText}`);
+                            }
                         }
                         // Hash the contents.
                         const cacheBustedHash = sha1Binary(yield cacheBustedResult.clone().arrayBuffer());
@@ -1536,6 +1547,18 @@
                 // Next, check if this is a navigation request for a route. Detect circular
                 // navigations by checking if the request URL is the same as the index URL.
                 if (this.adapter.normalizeUrl(req.url) !== this.indexUrl && this.isNavigationRequest(req)) {
+                    if (this.manifest.navigationRequestStrategy === 'freshness') {
+                        // For navigation requests the freshness was configured. The request will always go trough
+                        // the network and fallback to default `handleFetch` behavior in case of failure.
+                        try {
+                            return yield this.scope.fetch(req);
+                        }
+                        catch (_a) {
+                            // Navigation request failed - application is likely offline.
+                            // Proceed forward to the default `handleFetch` behavior, where
+                            // `indexUrl` will be requested and it should be available in the cache.
+                        }
+                    }
                     // This was a navigation request. Re-enter `handleFetch` with a request for
                     // the URL.
                     return this.handleFetch(this.adapter.newRequest(this.indexUrl), context);
@@ -2207,6 +2230,9 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
                     res = yield appVersion.handleFetch(event.request, event);
                 }
                 catch (err) {
+                    if (err.isUnrecoverableState) {
+                        yield this.notifyClientsAboutUnrecoverableState(appVersion, err.message);
+                    }
                     if (err.isCritical) {
                         // Something went wrong with the activation of this version.
                         yield this.versionFailed(appVersion, err);
@@ -2717,6 +2743,23 @@ ${msgIdle}`, { headers: this.adapter.newHeaders({ 'Content-Type': 'text/plain' }
                 hash,
                 appData: manifest.appData,
             };
+        }
+        notifyClientsAboutUnrecoverableState(appVersion, reason) {
+            return __awaiter(this, void 0, void 0, function* () {
+                const broken = Array.from(this.versions.entries()).find(([hash, version]) => version === appVersion);
+                if (broken === undefined) {
+                    // This version is no longer in use anyway, so nobody cares.
+                    return;
+                }
+                const brokenHash = broken[0];
+                const affectedClients = Array.from(this.clientVersionMap.entries())
+                    .filter(([clientId, hash]) => hash === brokenHash)
+                    .map(([clientId]) => clientId);
+                affectedClients.forEach((clientId) => __awaiter(this, void 0, void 0, function* () {
+                    const client = yield this.scope.clients.get(clientId);
+                    client.postMessage({ type: 'UNRECOVERABLE_STATE', reason });
+                }));
+            });
         }
         notifyClientsAboutUpdate(next) {
             return __awaiter(this, void 0, void 0, function* () {
